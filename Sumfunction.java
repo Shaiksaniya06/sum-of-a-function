85bcc1dc98b848a4313bf0d4ee62a252a7644dd2
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 13:49:59 +0530

    Add files via upload

---
+++ Sumfunction.java
@@ -0,0 +1,15 @@
+public class Sumfunction {
+    public static int addnumbers(int a,int b){
+        return a+b;
+    }
+    public static void main(String[]args){
+        int num1 =10;
+        int num2 =20;
+        int sum=addnumbers(num1,num2);
+        System.out.println("the sum of"+num1+"and"+num2+"is:"+sum);
+    }
+}
+    
+
+    
+
